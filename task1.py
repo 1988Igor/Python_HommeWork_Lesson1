f7a7d379015ac4866150ada13f1a01b40254ec77
Igor <romanigor.conect@gmail.com>
2022-10-09 00:15:51 +0200

    added some corections

--- task1.py
+++ task1.py
@@ -1,6 +1,5 @@
 #Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 number_day = int(input('Enter a number of day'))
-
 if(number_day <= 5):
     print('No, this day is not Weekend')
 elif(number_day <= 7):
--- task2.py
+++ task2.py
@@ -1,15 +1,14 @@
 #Напишите программу, которая принимает на вход координаты 
 # точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
-
 coordinates_x =int(input('Enter the coordinates for \"x"'))
 coordinates_y =int(input('Enter the coordinates for \"y"'))
 if (coordinates_x==0 and coordinates_y==0):
     print('Please introduce the coordinate biger than \"0" ')
 elif(coordinates_x>0 and coordinates_y>0):
-    print('quarter 1')
+    print('Quarter 1')
 elif(coordinates_x<0 and coordinates_y>0):
-    print('quarter 2')
+    print('Quarter 2')
 elif(coordinates_x<0 and coordinates_y<0):
-    print('quarter 3')
+    print('Quarter 3')
 elif(coordinates_x>0 and coordinates_y<0):
-    print('quarter 4')
+    print('Quarter 4')
